c407ca5b7beea368e61441fa8a6f2b839672b948
Ricardo Bartels <ricardo.bartels@telekom.de>
2026-01-08 09:58:07 +0100

    add support for formbricks blocks

--- formbricks/handler.py
+++ formbricks/handler.py
@@ -1,13 +1,59 @@
+from typing import List
+
 from app.lib import strip_tags, grab
 from app.models import InternalWebhookContent, InternalWebhookField
 from formbricks.client import FormbricksClient
 from formbricks.models import FormbricksWebhook
 
 
-def normalize_webhook_content(content: FormbricksWebhook, client: FormbricksClient) -> InternalWebhookContent:
+def convert_question(survey_question: dict, answers: dict) -> List[InternalWebhookField]:
 
     # need to stay in this order to parse answer correctly
     contact_info_items = ["firstName", "lastName", "email", "phone", "company"]
+
+    question_id = survey_question.get("id")
+    question_title = strip_tags(grab(survey_question, "headline.default", fallback=""))
+    question_type = survey_question.get("type")
+    question_column_type = "Text"
+
+    question_answer = answers.get(question_id)
+
+    return_data = list()
+    if question_answer is None:
+        return return_data
+
+    if question_type == "contactInfo":
+
+        for key, part in enumerate(contact_info_items):
+            return_data.append(
+                InternalWebhookField(
+                    id=f"{question_id}_{part}",
+                    label=f'{question_title} - {grab(survey_question, f"{part}.placeholder.default")}',
+                    type=question_column_type,
+                    value=strip_tags(question_answer[key])
+                )
+            )
+
+    else:
+
+        if question_type == "date":
+            question_column_type = "Date"
+        elif question_type == "multipleChoiceMulti":
+            question_answer = ", ".join(question_answer)
+
+        return_data.append(
+            InternalWebhookField(
+                id=question_id,
+                label=question_title,
+                type=question_column_type,
+                value=strip_tags(question_answer)
+            )
+        )
+
+    return return_data
+
+
+def normalize_webhook_content(content: FormbricksWebhook, client: FormbricksClient) -> InternalWebhookContent:
 
     return_item = InternalWebhookContent(
         survey_id=content.data.surveyId,
@@ -24,41 +70,11 @@ def normalize_webhook_content(content: FormbricksWebhook, client: FormbricksClie
     # iterate over questions
     for survey_question in grab(survey_data, "data.questions") or list():
 
-        question_id = survey_question.get("id")
-        question_title = strip_tags(grab(survey_question, "headline.default", fallback=""))
-        question_type = survey_question.get("type")
-        question_column_type = "Text"
+        return_item.data.extend(convert_question(survey_question, content.data.data))
 
-        question_answer = content.data.data.get(question_id)
+    for survey_blocks in grab(survey_data, "data.blocks") or list():
 
-        if question_answer is None:
-            continue
-
-        if question_type == "contactInfo":
-
-            for key, part in enumerate(contact_info_items):
-                return_item.data.append(
-                    InternalWebhookField(
-                        id=f"{question_id}_{part}",
-                        label=f'{question_title} - {grab(survey_question, f"{part}.placeholder.default")}',
-                        type=question_column_type,
-                        value=question_answer[key]
-                    )
-                )
-        else:
-
-            if question_type == "date":
-                question_column_type = "Date"
-            elif question_type == "multipleChoiceMulti":
-                question_answer = ", ".join(question_answer)
-
-            return_item.data.append(
-                InternalWebhookField(
-                    id=question_id,
-                    label=question_title,
-                    type=question_column_type,
-                    value=question_answer
-                )
-            )
+        for block_question in survey_blocks.get("elements") or list():
+            return_item.data.extend(convert_question(block_question, content.data.data))
 
     return return_item
